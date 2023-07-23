b384bc5a35de1f6615d2a81423d44d83248876b4
Daniel <d.alnemri@mail.utoronto.ca>
2023-07-22 21:42:56 -0400

    Update article.ts

--- src/services/article.ts
+++ src/services/article.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { FetchBaseQueryError } from '@reduxjs/toolkit/dist/query/react';
 import process from 'process';
 
-const {RAPID_API_KEY} = process.env;//import.meta.env.RAPID_API_KEY;
+const apiKey = process.env.RAPID_API_KEY;//import.meta.env.RAPID_API_KEY;
 
 export const isFetchBaseQueryErrorType = (error: any): error is FetchBaseQueryError => 'status' in error
 
@@ -11,7 +11,7 @@ export const articleApi = createApi({
     baseQuery: fetchBaseQuery({ 
         baseUrl: 'https://article-extractor-and-summarizer.p.rapidapi.com/',
         prepareHeaders: (headers) => {
-        headers.set('X-RapidAPI-Key', `${RAPID_API_KEY}`);
+        headers.set('X-RapidAPI-Key', `${apiKey}`);
         headers.set('X-RapidAPI-Host', 'article-extractor-and-summarizer.p.rapidapi.com');
 
         return headers;
